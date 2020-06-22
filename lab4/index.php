b7b46765d30b51db91f5dede7440b9c5a5ec9436
Artem Borisov <artpl111222@yandex.ru>
2020-06-22 22:23:06 +0300

    Lab 4 fix

--- lab4/index.php
+++ lab4/index.php
@@ -12,25 +12,25 @@ $router->get("/", new class implements IRequestHandler {
    public function handle(Request $req) {
        if (!is_null($req->getRequestQueryValueByKey("id"))) {
            switch ($req->getRequestQueryValueByKey("id")) {
-               case "form" : print(file_get_contents($_SERVER['DOCUMENT_ROOT']."/inc/table.inc.html")); break;
-               case "table" : print(file_get_contents($_SERVER['DOCUMENT_ROOT']."/inc/form.inc.html")); break;
-               default : print(file_get_contents($_SERVER['DOCUMENT_ROOT']."/inc/table.inc.html")); break;
+               case "form" : print(file_get_contents($_SERVER['DOCUMENT_ROOT']."/inc/table.html")); break;
+               case "table" : print(file_get_contents($_SERVER['DOCUMENT_ROOT']."/inc/form.html")); break;
+               default : print(file_get_contents($_SERVER['DOCUMENT_ROOT']."/inc/table.html")); break;
            }
        } else {
-		   print(file_get_contents($_SERVER['DOCUMENT_ROOT']."/inc/index.inc.html"));
+		   print(file_get_contents($_SERVER['DOCUMENT_ROOT']."/inc/index.html"));
 	   }
    }
 });
 
 $router->get("/form", new class implements IRequestHandler {
     public function handle(Request $req) {
-        print(file_get_contents($_SERVER['DOCUMENT_ROOT']."/inc/form.inc.html"));
+        print(file_get_contents($_SERVER['DOCUMENT_ROOT']."/inc/form.html"));
     }
 });
 
 $router->get("/table", new class implements IRequestHandler {
     public function handle(Request $req) {
-        print(file_get_contents($_SERVER['DOCUMENT_ROOT']."/inc/table.inc.html"));
+        print(file_get_contents($_SERVER['DOCUMENT_ROOT']."/inc/table.html"));
     }
 });
 
